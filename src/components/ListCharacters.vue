56cff8f6296702c5a617ffeb6d947cc64092b9f9
Rafael Ramirez <susurrus.ater@gmail.com>
2021-12-19 15:19:40 -0300

    Character card

--- src/components/ListCharacters.vue
+++ src/components/ListCharacters.vue
@@ -1,26 +1,43 @@
 <template>
   <section>
     <div class="characters">
-      <div class="characters_item">
         <h2>Characters</h2>
+      <div class="characters_item" v-for="character in characters" :key="character.id">
+        <CharacterCard :character="character"/>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import {onMounted} from 'vue'
-//para acceder a todo el store configurado
+import {onMounted, computed} from 'vue'
+//para acceder a todo el store configurado con vuex
 import {useStore} from 'vuex'
+//importamos nuestro componente para mostrar los personajes
+import CharacterCard from '@/components/CharacterCard'
 
 export default {
+
+    //y agregamos nuestro componente a nuestros components
+    components:{
+        CharacterCard
+    },
     setup(){
+        //agregamos todo el store a una variable, para luego acceder al él
         const store = useStore();
+        //accedemos al characters que se encuentra en el store
+        //con computed para que realice la acción
+        const characters = computed(()=> store.state.charactersFilter)
 
         onMounted(() => {
-            //con dispatch accedemos a las actions del store
+            //con dispatch accedemos a las actions del store, en este caso llamamos la funcion getCharacters y se ejecuta cuando apenas la app este montada
             store.dispatch('getCharacters')
         })
+
+        return {
+            //retornamos el resultado a la app
+            characters
+        }
     }}
 
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { createStore } from 'vuex'
+//con createStore creamos todas las acciones, variables
+//con useStore las usamos en los componentes que la requieran
 
 export default createStore({
   state: {
     characters: [],
     charactersFilter: []
   },
+
   mutations: {
     setCharacter(state, payload) {
       state.characters = payload
@@ -13,17 +16,22 @@ export default createStore({
       state.charactersFilter = payload
     }
   },
+
   actions: {
     async getCharacters({ commit }) {
       try {
         const response = await fetch("https://rickandmortyapi.com/api/character")
         const data = await response.json()
-        console.log(data);
+        //el commit es nuestro disparador para ejecutar la acción
+        //esto actualiza el estado de nuestras variables, las muta
+        commit('setCharacter', data.results)
+        commit('setCharacterFilter', data.results)
       } catch (error) {
         console.error(error)
       }
     }
   },
+
   modules: {
   }
 })
